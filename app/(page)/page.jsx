743e222fdb19352ffd0135f9dd2c1cab1755e033
Stéphanie Maminiaina <stephaniepageot42@gmail.com>
2025-01-26 15:30:47 +0100

    Add React Cookie Bot for cookie consent management; update language to French

--- app/(page)/page.jsx
+++ app/(page)/page.jsx
@@ -1,23 +1,34 @@
+"use client";
+
+import ReactCookieBot from "react-cookiebot";
 import Annonce from "@/components/MainComponents/Sections/Annonce/Annonce";
 import Faq from "@/components/MainComponents/Sections/Faq/Faq";
 import Hero from "@/components/MainComponents/Sections/Hero/Hero";
 import Informations from "@/components/MainComponents/Sections/Informations/Informations";
-import Partenaires from "@/components/MainComponents/Sections/Partenaires/Partenaires";
 import Sponsors from "@/components/MainComponents/Sections/Sponsors/Sponsors";
 import { Testimonial } from "@/components/MainComponents/Sections/Testimonials/Testimonials";
 import React from "react";
 
+const domainGroupId = "8877150d-f4a3-4413-adeb-12dcd85cc3b4";
+
 const Home = () => {
   return (
-    <main className="p-8">
-      <Hero />
-      <Annonce />
-      <Informations />
-      <Faq />
-      <Testimonial />
-      <Sponsors />
-      {/* <Partenaires /> */}
-    </main>
+    <div>
+      <ReactCookieBot
+        domainGroupId={domainGroupId}
+        language="FR"
+        data-lang="fr"
+      />
+
+      <main className="p-8">
+        <Hero />
+        <Annonce />
+        <Informations />
+        <Faq />
+        <Testimonial />
+        <Sponsors />
+      </main>
+    </div>
   );
 };
 
